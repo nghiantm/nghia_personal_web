022fff52d509edd84cf7b2dcbf4ae96cf4d4df5a
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2024-02-21 00:27:19 -0500

    Added avatar to homepage, replaced vite.svg with my face

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import { CssBaseline, Grid, Typography, useTheme } from "@mui/material";
 import React from "react";
+import { Img } from "react-image";
 
 const Home = () => {
     const styles = {
@@ -7,7 +8,16 @@ const Home = () => {
             fontWeight: 700
         },
         introItem: {
-            mt: '6.25rem'
+            mt: '6.25rem',
+        },
+        avatarItem: {
+            mt: '4rem',
+            display: 'flex',
+            alignItems: 'center'
+        },
+        avatarText: {
+            ml: '1rem',
+            color: '#73737d'
         }
     }
 
@@ -18,6 +28,18 @@ const Home = () => {
                 <Grid item xs={12} sm={9} md={7} sx={styles.introItem}>
                     <Typography variant='h1' align='left' sx={styles.introText}>
                         <span style={styles.span}>Hi, I am Nghia</span>. I live in Philly, USA. I am a software engineer experimenting with <span style={styles.span}>Web Technologies</span>.
+                    </Typography>
+                </Grid>
+
+                <Grid item xs={12} sm={9} sx={styles.avatarItem}>
+                    <Img 
+                        src={'public/nghia.jpeg'}
+                        width={"46"}
+                        style={{borderRadius: 25}}
+                    />
+
+                    <Typography variant='body1' sx={styles.avatarText}>
+                        Based in Philly, US. SWE in the making.
                     </Typography>
                 </Grid>
             </Grid>
